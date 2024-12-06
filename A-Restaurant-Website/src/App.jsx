febb52e5915ec8023ad388e2ae695f483fdbe3b3
Tushar Saini <tsaini425@gmail.com>
2024-12-06 18:14:40 +0530

    Add Restaurant Summary

--- A-Restaurant-Website/src/App.jsx
+++ A-Restaurant-Website/src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
 import Headers from "./Components/Layout/header";
+import Meals from "./Components/Meals/Meals";
 
 function App() {
   return (
     <>
       <Headers />
+      <main>
+        <Meals />
+      </main>
     </>
   );
 }
