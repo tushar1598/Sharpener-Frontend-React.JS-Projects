94c3674eaf419fd439de77ed0fad7153d4429341
Tushar Saini <tsaini425@gmail.com>
2024-12-06 11:23:53 +0530

    cd Sharpener-Frontend-ReactJS-Projects

---
+++ A-Restaurant-Website/src/Components/Layout/header.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import styles from "./header.module.css";
+
+const Headers = () => {
+  return (
+    <>
+      <header className={styles.header}>
+        <h1>ReactMeals</h1>
+        <button className={styles.cartButton}>
+          <span className={styles.cartIcon}>🛒</span>
+          <span>Your Cart</span>
+          <span className={styles.cartBadge}>0</span>
+        </button>
+      </header>
+      <div className={styles["main-image"]}>
+        <img
+          src="https://images2.alphacoders.com/100/1003810.jpg"
+          alt="A table full of delicious food!"
+        />
+      </div>
+    </>
+  );
+};
+
+export default Headers;
