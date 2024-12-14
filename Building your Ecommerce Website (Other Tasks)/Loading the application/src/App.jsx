d7a2d884545d471e13b6f9de2ccbfb03cffecb01
Tushar Saini <tsaini425@gmail.com>
2024-12-14 16:31:43 +0530

    Loading the application

---
+++ Building your Ecommerce Website (Other Tasks)/Loading the application/src/App.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import MoviesList from "./Components/MovieList";
+import "./App.css";
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsloading] = useState(false);
+
+  const fetchMoviesHandler = async () => {
+    setIsloading(true);
+    const response = await fetch("https://swapi.dev/api/films");
+    const data = await response.json();
+    console.log(data);
+    const transformedMovies = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date,
+      };
+    });
+    setMovies(transformedMovies);
+    setIsloading(false);
+  };
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Found No Movies!</p>}
+        {isLoading && <p>Loading...</p>}
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
