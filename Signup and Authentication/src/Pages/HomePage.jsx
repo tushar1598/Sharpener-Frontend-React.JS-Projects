24146b0f9c7a1192a8e91003324a570e86394d79
Tushar Saini <107983754+tushar1598@users.noreply.github.com>
2024-12-15 02:47:06 +0530

    Delete Signup and Authentication directory

--- Signup and Authentication/src/Pages/HomePage.jsx
+++
@@ -1,7 +0,0 @@
-import StartingPageContent from "../Components/StartingPage/StartingPageContent";
-
-const HomePage = () => {
-  return <StartingPageContent />;
-};
-
-export default HomePage;
--- Signup and Authentication/src/Pages/ProfilePage.jsx
+++
@@ -1,7 +0,0 @@
-import UserProfile from "../Components/Profile/UserProfile";
-
-const ProfilePage = () => {
-  return <UserProfile />;
-};
-
-export default ProfilePage;
