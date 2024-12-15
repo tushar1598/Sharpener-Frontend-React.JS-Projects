4b4ebde0bc6edc8d5a36b0f64702168431e7f019
Tushar Saini <tsaini425@gmail.com>
2024-12-16 00:04:40 +0530

    Implementing Logout functionality

---
+++ Building your Ecommerce Website (Other Tasks)/Implementing Logout functionality/src/Components/Layout/MainNavigation.jsx
@@ -0,0 +1,45 @@
+import { Link, useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import styles from "./MainNavigation.module.css";
+import AuthContext from "../../store/AuthContext";
+
+const MainNavigation = () => {
+  const nevigate = useNavigate();
+  const authContext = useContext(AuthContext);
+
+  const isLoggedIn = authContext.isLoggedIn;
+
+  const logoutHandler = () => {
+    authContext.logout();
+    nevigate("/");
+  };
+
+  return (
+    <header className={styles.header}>
+      <Link to="/">
+        <div className={styles.logo}>React Auth</div>
+      </Link>
+      <nav>
+        <ul>
+          {!isLoggedIn && (
+            <li>
+              <Link to="/auth">Login</Link>
+            </li>
+          )}
+          {isLoggedIn && (
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+          )}
+          {isLoggedIn && (
+            <li>
+              <button onClick={logoutHandler}>Logout</button>
+            </li>
+          )}
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainNavigation;
