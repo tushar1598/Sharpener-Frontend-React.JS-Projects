e25bee7e5e891b9612e7c2e54291e1600988b6aa
Tushar Saini <tsaini425@gmail.com>
2024-12-16 02:22:40 +0530

    Protecting the product page

--- Building your Ecommerce Website/src/Components/Routers/Router.jsx
+++ Building your Ecommerce Website/src/Components/Routers/Router.jsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useC, useContext } from "react";
 import ProductList from "../Products/ProductLists";
 import About from "../Layout/About";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import Home from "../Layout/Home";
 import ContactUs from "../Layout/ContactUs";
 import ProductPage from "../Products/ProductPage";
 import AuthForm from "../Auth/AuthForm";
+import AuthContext from "../Store/AuthContaxt";
 
 export const routePath = {
   Home: "/",
@@ -17,14 +18,48 @@ export const routePath = {
 };
 
 const Routers = () => {
+  const authContext = useContext(AuthContext);
+
   return (
     <div>
       <Routes>
         <Route path={routePath.Home} element={<Home />} />
-        <Route path={routePath.Store} element={<ProductList />} />
-        <Route path={routePath.ProductPage} element={<ProductPage />} />
+
+        <Route
+          path={routePath.Store}
+          element={
+            authContext.isLoggedIn ? (
+              <ProductList />
+            ) : (
+              <Navigate to={routePath.Login} />
+            )
+          }
+        />
+
+        <Route
+          path={routePath.ProductPage}
+          element={
+            authContext.isLoggedIn ? (
+              <ProductPage />
+            ) : (
+              <Navigate to={routePath.Login} />
+            )
+          }
+        />
+
         <Route path={routePath.About} element={<About />} />
-        <Route path={routePath.Login} element={<AuthForm />} />
+
+        <Route
+          path={routePath.Login}
+          element={
+            authContext.isLoggedIn ? (
+              <Navigate to={routePath.Store} />
+            ) : (
+              <AuthForm />
+            )
+          }
+        />
+
         <Route path={routePath.ContactUs} element={<ContactUs />} />
       </Routes>
     </div>
