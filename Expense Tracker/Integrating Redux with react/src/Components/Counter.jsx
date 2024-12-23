9ca39b937d7895aaaa05b47a8deb99e16d72b3dc
Tushar Saini <tsaini425@gmail.com>
2024-12-23 11:43:11 +0530

    Integrating Redux with react

---
+++ Expense Tracker/Integrating Redux with react/src/Components/Counter.jsx
@@ -0,0 +1,14 @@
+import styles from "./Counter.module.css";
+
+const Counter = () => {
+  const toggleCounterHandler = () => {};
+  return (
+    <main className={styles.counter}>
+      <h1>Redux Counter</h1>
+      <div className={styles.value}>-- COUNTER VALUE --</div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+
+export default Counter;
