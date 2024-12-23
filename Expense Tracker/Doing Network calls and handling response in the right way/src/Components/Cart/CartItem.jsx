fb271a017cf604166cb6b281b6506a7d83c9e49c
Tushar Saini <tsaini425@gmail.com>
2024-12-24 00:13:08 +0530

    Doing Network calls and andling response the right way

---
+++ Expense Tracker/Doing Network calls and handling response in the right way/src/Components/Cart/CartItem.jsx
@@ -0,0 +1,45 @@
+import styles from "./CartItem.module.css";
+import { useDispatch } from "react-redux";
+import { cartActions } from "../../Store/Cart-Slice";
+
+const CartItem = (props) => {
+  const dispatch = useDispatch();
+  const { title, quantity, total, price, id } = props.item;
+
+  const removeItemHandler = () => {
+    dispatch(cartActions.removeItemFromCart(id));
+  };
+
+  const addItemHandler = () => {
+    dispatch(
+      cartActions.addItemToCart({
+        id,
+        title,
+        price,
+      })
+    );
+  };
+
+  return (
+    <li className={styles.item}>
+      <header>
+        <h3>{title}</h3>
+        <div className={styles.price}>
+          ${total.toFixed(2)}{" "}
+          <span className={styles.itemprice}>(${price.toFixed(2)}/item)</span>
+        </div>
+      </header>
+      <div className={styles.details}>
+        <div className={styles.quantity}>
+          x <span>{quantity}</span>
+        </div>
+        <div className={styles.actions}>
+          <button onClick={removeItemHandler}>-</button>
+          <button onClick={addItemHandler}>+</button>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default CartItem;
